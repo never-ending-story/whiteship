3643548525192fcdd92707480312f4f6a5b0357b
Kim nayeon <kimnayeon0108@gmail.com>
2021-08-12 18:07:33 +0900

    [yeon] Week5 - Binary Tree 과제

---
+++ week5/yeon/Node.java
@@ -0,0 +1,30 @@
+public class Node {
+
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
